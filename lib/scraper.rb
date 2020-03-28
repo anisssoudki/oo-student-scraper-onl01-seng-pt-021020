074bb8037e72bdae302ac83e73d1a7dc2b898170
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-28 22:39:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,28 +3,7 @@ require 'pry'
 
 class Scraper
 
-#     def self.scrape_index_page(index_url)
-#       s = []
-#     students = []
-#     student_details = {}
-#     html = open(index_url)
-#     index = Nokogiri::HTML(html)
-#     counter = 0 
-   
-#   array_of_names =  index.css("div.student-card").css("a").css("div.card-text-container").css("h4.student-name").children
-#   while counter < array_of_names.length
-#   students <<  array_of_names[counter].text
-#   counter = counter + 1 
-   
-# end
-# students
-# student_details[:name] = students[0]
-# s << student_details
-# student_details[:name] = students[1]
-# p student_details
-# p s
-#   end
- def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url)
     students = []
     html = open(index_url)
     index = Nokogiri::HTML(html)
@@ -36,11 +15,10 @@ class Scraper
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
       students << student_details
     end
-    p students
+    students
   end
-  
 
-ef self.scrape_profile_page(profile_url)
+ def self.scrape_profile_page(profile_url)
     student_profile = {}
     html = open(profile_url)
     profile = Nokogiri::HTML(html)
@@ -65,5 +43,3 @@ ef self.scrape_profile_page(profile_url)
     student_profile
   end
 end
-end
-
