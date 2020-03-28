3a0a0aac6372d8320e4341fd15a216fa713a6e9b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-28 22:47:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
   
-      student_details[:profile_url] =  student.css("a").css("href").value
+      student_details[:profile_url] =  student.css("a").css("href")
       students << student_details
     end
     students
