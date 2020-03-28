b129af1ec63491dfeea84c1b785bb3d33a8bfb6b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-28 22:35:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
 student_details[:name] = students[0]
 s << student_details
 student_details[:name] = students[1]
-
+p student_details
   end
 
   def self.scrape_profile_page(profile_url)
