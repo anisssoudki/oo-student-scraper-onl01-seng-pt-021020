d12b67d9eaea783a1cd17e5fc7fcaf3ee66fc0e0
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-28 22:30:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,30 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+    def self.scrape_index_page(index_url)
+      s = []
+    students = []
+    student_details = {}
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    counter = 0 
+   
+  array_of_names =  index.css("div.student-card").css("a").css("div.card-text-container").css("h4.student-name").children
+   while counter < array_of_names.length
+   students <<  array_of_names[counter].text
+   counter = counter + 1 
+   
+ end
+p students
+index = 0 
+  while index < students.length do 
+     student_details[:name]=[students[index]] 
+   s << student_details
+   index = index + 1 
+end
+  p s
+ 
+ 
   end
 
   def self.scrape_profile_page(profile_url)
