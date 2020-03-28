2624a7085c065467dd41733caf19d2928ca1485e
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-28 22:36:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,27 +3,27 @@ require 'pry'
 
 class Scraper
 
-    def self.scrape_index_page(index_url)
-      s = []
-    students = []
-    student_details = {}
-    html = open(index_url)
-    index = Nokogiri::HTML(html)
-    counter = 0 
+#     def self.scrape_index_page(index_url)
+#       s = []
+#     students = []
+#     student_details = {}
+#     html = open(index_url)
+#     index = Nokogiri::HTML(html)
+#     counter = 0 
    
-  array_of_names =  index.css("div.student-card").css("a").css("div.card-text-container").css("h4.student-name").children
-   while counter < array_of_names.length
-   students <<  array_of_names[counter].text
-   counter = counter + 1 
+#   array_of_names =  index.css("div.student-card").css("a").css("div.card-text-container").css("h4.student-name").children
+#   while counter < array_of_names.length
+#   students <<  array_of_names[counter].text
+#   counter = counter + 1 
    
- end
- students
-student_details[:name] = students[0]
-s << student_details
-student_details[:name] = students[1]
-p student_details
-p s
-  end
+# end
+# students
+# student_details[:name] = students[0]
+# s << student_details
+# student_details[:name] = students[1]
+# p student_details
+# p s
+#   end
 
   def self.scrape_profile_page(profile_url)
     
